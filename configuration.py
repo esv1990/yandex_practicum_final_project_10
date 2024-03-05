961093389856345cb4fe7df008d73dbf993e705f
sergs <sergserg081990@yandex.ru>
2024-03-05 22:45:05 +0300

    рестарт. заменил юрл

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://8f32ef27-bbbb-4460-b03b-a7bcc82d6397.serverhub.praktikum-services.ru" # вставить ссылку с тренажера
+URL_SERVICE = "https://01abba0b-bb7c-43fa-8248-e28e50a5f8b0.serverhub.praktikum-services.ru" # вставить ссылку с тренажера
 CREATE_PRODUCTS_KIT_PATH = "/api/v1/kits"
 CREATE_USER_PATH = "/api/v1/users/"
